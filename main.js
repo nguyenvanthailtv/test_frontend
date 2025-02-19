c169c0f48f1bd466baf55c80011f3df05c8d70c1
nguyen van thai <nguyenvanthailtv@gmail.com>
2025-02-19 10:21:20 +0700

    fix ui

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const products = document.querySelectorAll('.product-item__top')
 products.forEach((item) => {
     item.addEventListener('click', (e) => {
         const productShow = document.querySelector('.product-item.show')
-        if(productShow) productShow.classList.remove('show')
+        if(productShow && !item.parentElement.classList.contains('show')) productShow.classList.remove('show')
         item.parentElement?.classList.toggle('show')
     })
 })
